68aa4cccac026ece3c9c28115da08c0c6fbf25da
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2022-12-05 23:01:26 +0100

    Basic Test
    return i^n for positive Integer, None otherwise alse return None in case of errors

--- src/main/java/basic/BasicTest.java
+++ src/main/java/basic/BasicTest.java
@@ -12,6 +12,21 @@ public class BasicTest {
    * alse return None in case of errors
    */
   public static Option<Integer> power(Integer i, Integer n) {
-    return Option.none();
+    if (n==0){  
+      return Option.some(1);
+    }else if(i==0){
+      return Option.some(0);
+    }else if(i>0){
+      int product = 1;
+      if (i>=0){
+        for (int j=0; j<n; j++){
+          product *= i;
+        }
+      }
+      if (product == 0) return Option.none();
+      return Option.some(product);
+    }else{
+      return Option.none();
+    }
   }
 }
