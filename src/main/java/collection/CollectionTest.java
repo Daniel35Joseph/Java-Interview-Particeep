030aecfc9ad7fb901318e758cfb2b6dcecb91cb1
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2022-12-05 22:38:30 +0100

    Collection Test 2
    operation : abc -> AbcAbc
    List<String>

--- src/main/java/collection/CollectionTest.java
+++ src/main/java/collection/CollectionTest.java
@@ -13,23 +13,27 @@ import java.util.ArrayList;
 class CollectionTest {
 
 
-  /**
-   * operation : x -> ((x * 2) + 3) ^ 5
-   */
-  public static List<Double> compute1(List<Integer> input) {
-    List<Double> output = new ArrayList<>();
-    for(int i=0 ; i<input.size() ; i++){
-      double x = input.get(i);
-      output.add(Math.pow(((x * 2) + 3),5));
-    }
-    return output;
-  }
-
-  /**
+   /**
    * operation : abc -> AbcAbc
    */
   public static List<String> compute2(List<String> input) {
-    return null;
+    List<String> output = new ArrayList<>();
+    
+    
+    for(int i=0 ; i<input.size() ; i++){
+      String text = input.get(i);
+      if (text.length() == 1){
+        text=text.toUpperCase();
+      }else if(text.length() == 0){
+        
+      }else{
+        String firstLetter = text.substring(0, 1).toUpperCase();
+        text = firstLetter+text.substring(1);
+      }
+      //text = text+text;
+      output.add(text+text);
+    } 
+    return output;
   }
 
 }
